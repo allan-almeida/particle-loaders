9c0e50a2f9e4779a6d8725ae1100e841b91c981f
Allan Almeida <allan.almeida91@gmail.com>
2018-05-07 18:22:25 +1000

    created basic holding page

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,12 +1,50 @@
 import React from 'react'
-import Navbar from '../../components/Navbar'
+import styled from 'styled-components'
+import { media } from '../../styles'
+
+const Background = styled.div`
+  height: 100vh;
+  background: linear-gradient(to left top, #ECE9E6, #FFF);
+`
+
+const LeftColumn = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  flex-direction: column;
+  height: 100%;
+  width: ${100 / 3}vw;
+  ${media.desktop`
+    width: 100vw;
+    align-items: center;
+  `}
+`
+
+const HeroTitle = styled.h1`
+  font-size: 3em;
+  font-weight: 100;
+  margin: 0;
+`
+
+const SubTitle = styled.h2`
+  font-weight: 100;
+`
+
+const MailLink = styled.a`
+  text-decoration: none;
+  color: black;
+`
 
 class Home extends React.Component {
   render () {
     return (
-      <div>
-        <Navbar />
-      </div>
+      <Background>
+        <LeftColumn>
+          <HeroTitle>Allan Almeida</HeroTitle>
+          <SubTitle>Web Development</SubTitle>
+          <MailLink href="mailto:allan.almeida91@gmail.com?Subject=Contact%20Allan" target="_top">Contact</MailLink>
+        </LeftColumn>
+      </Background>
     )
   }
 }
